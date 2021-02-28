c0793b061b550e3a734ad04eb4640ed5696effb1
Laurinda Zhang <78317349+LaurindaZhang@users.noreply.github.com>
2021-02-28 01:13:28 -0600

    Add files via upload
    crawl restaurant name, zip_code, num_review, rating; save to pandas dataframe and/or csv

---
+++ Crawler_Laurinda_02280112.py
@@ -0,0 +1,54 @@
+# Crawling data from Yelp
+
+import re
+import util
+import bs4
+import queue
+import json
+import sys
+import csv
+import pandas as pd
+
+
+def get_info(links):
+    '''
+    Extract restaurants info
+    '''
+    info = []
+
+    for link in links:
+        restaurant = {}
+        request = util.get_request(url)
+        text = util.read_request(request)
+        soup = bs4.BeautifulSoup(text, "html5lib")
+        tag = soup.find('script', type='application/ld+json')
+        d = json.loads(tag.text)
+
+        restaurant['name'] = d['name']
+        restaurant['price_range'] = d['priceRange']         #'$11-30'
+        restaurant['cuisine'] = d['servesCuisine']          #'Italian'
+        restaurant['num_review'] = len(d["review"])         # Num of reviews
+        
+        reviews  = d["review"]
+        rating = 0
+        for review in reviews:
+            rating += review['reviewRating']['ratingValue']
+        restaurant['rating'] = rating/len(d["review"])
+
+        info.append(restaurant)
+
+    return info
+
+def load_to_df(info):
+    '''
+    Load restaurants info (list of dicts) to pandas dataframe
+    '''
+    return pd.DataFrame.from_dict(info, orient='columns')
+
+def to_csv(df):
+    '''
+    Load pandas dataframe to csv
+    '''
+    return df.to_csv('info.csv', index=False)
+
+
